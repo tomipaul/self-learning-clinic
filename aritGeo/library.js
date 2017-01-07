0ec56be162df0dcb9914c068e958adfbde14c591
Oluwatomi Akande <tomipaul95@gmail.com>
2017-01-07 06:10:01 +0100

    implemented aritGeo function

--- aritGeo/library.js
+++ aritGeo/library.js
@@ -1,4 +1,24 @@
 function aritGeo(numberArray) {
-	return
+	if (numberArray.length == 0) {
+    return 0;
+  }
+  var [ratio, difference] = [(numberArray[1]/numberArray[0]), (numberArray[1]-numberArray[0])];
+  for(var index=1; index<numberArray.length-1; index++) {
+    if (numberArray[index+1]/numberArray[index] != ratio) {
+      break;
+    }
+    if (index == numberArray.length-2) {
+      return "Geometric";
+    }
+  }
+  for(var index=1; index < numberArray.length-1; index++) {
+    if (numberArray[index+1]-numberArray[index] != difference) {
+      break;
+    }
+    if (index == numberArray.length-2) {
+      return "Arithmetic";
+    }
+  }
+  return -1;
 }
 module.exports = aritGeo;
