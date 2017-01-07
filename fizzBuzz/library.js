8cf3c7350515c145de210c5e8a5acd1e528f7b3a
Oluwatomi Akande <tomipaul95@gmail.com>
2017-01-07 04:31:04 +0100

    Implemented function fizzBuzz

--- fizzBuzz/library.js
+++ fizzBuzz/library.js
@@ -1,4 +1,22 @@
 function fizzBuzz(n) {
-  return
+  if (!Number.isInteger(n) || n < 0) {
+    return "ValueError, Positive Integer input required"
+  }
+  if (n == 0) {
+    return 0;
+  }
+  var fizz_buzz = '';
+  if (n % 3 == 0) {
+    fizz_buzz+='Fizz';
+  }
+  if (n % 5 == 0) {
+    fizz_buzz+='Buzz';
+  }
+  if (fizz_buzz) {
+    return fizz_buzz
+  }
+  else {
+    return n;
+  }
 }
 module.exports = fizzBuzz
