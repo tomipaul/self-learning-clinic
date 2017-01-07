b8bb94b196d3d314a9946f4e82d243dfaa524c71
Oluwatomi Akande <tomipaul95@gmail.com>
2017-01-07 03:12:27 +0100

    Added a second test suite with five test cases, tests the correctness of the algorithm with arrays of floats and integers; positive and negative.

--- findMinMax/tests.js
+++ findMinMax/tests.js
@@ -26,4 +26,28 @@
 
     });
 
+    describe('Return the min and max number in the array in a new array as follows `[min, max]`', function () {
+
+      it('should return [1,4] for [1, 2, 3 , 4]', function () {
+        expect(findMinMax([1, 2, 3, 4])).to.deep.equal([1, 4]);
+      });
+
+      it('should return [4, 6] for [6, 4]', function () {
+        expect(findMinMax([6, 4])).to.deep.equal([4, 6]);
+      });
+
+      it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
+        expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).to.deep.equal([2, 78]);
+      });
+
+      it('should return [-8, -2] for [-2, -3, -4, -5, -6, -7, -8]', function () {
+        expect(findMinMax([-2, -3, -4, -5, -6, -7, -8])).to.deep.equal([-8, -2]);
+      });
+
+      it('should return [4.52, 4.59] for [4.52, 4.53, 4.54, 4.55, 4.56, 4.57, 4.58, 4.59]', function() {
+        expect(findMinMax([4.52, 4.53, 4.54, 4.55, 4.56, 4.57, 4.58, 4.59])).to.deep.equal([4.52, 4.59]);
+      });
+
+    });
+
   });
